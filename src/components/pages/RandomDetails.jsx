cf6214b1a96722c6df83f12d322e37e23bcbd406
Flo <florian.aube@gmail.com>
2022-02-17 19:57:30 +0100

    renamed handler to service

--- src/components/pages/RandomDetails.jsx
+++ src/components/pages/RandomDetails.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 // import axios from 'axios';
-// import apiHandler from "./../../api/handler";
+// import service from "./../../api/service";
 
 class RandomDetails extends Component {
 	state = {
